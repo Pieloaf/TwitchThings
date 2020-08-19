d8b853dbb7ed93ca7acf50e3e87bdf00336ebc5b
Pieloaf <piercelowe1@gmail.com>
2020-08-19 01:30:02 +0100

    simplifying stuff

--- gif-overlay/gifs.js
+++
@@ -1,5 +0,0 @@
-// https://www.instafluff.tv/TwitchCustomRewardID/?channel=
-
-// IMPORTANT NOTE!!!! make sure to seperate each new addition with a ,
-gifs =
-  '[{"cheesecake": {"text": "Cheesecake!","Id": "Reward-ID"},"hugs": {"text": "(>^_^)> hugs for everyone!","Id": "Reward-ID"},"pat": {"text": "(^◇^)ﾉ(・_・) pat pat","Id": "Reward-ID"}}]';
--- gif-overlay/index.js
+++ gif-overlay/index.js
@@ -2,7 +2,17 @@ var block = document.getElementById("image");
 var gif = document.createElement("img");
 var text = document.createElement("h1");
 var sound = document.createElement("audio");
-var gifz = JSON.parse(gifs)[0];
+var gifz;
+
+fetch(
+  "https://raw.githubusercontent.com/Pieloaf/TwitchThings/master/gif-overlay/gifs.json"
+)
+  .then(function (resp) {
+    return resp.json();
+  })
+  .then(function (json) {
+    gifz = json;
+  });
 
 ComfyJS.onCommand = (user, command, message, flags, self, extra) => {
   if (flags.broadcaster && command === "reload") {
@@ -17,22 +27,10 @@ ComfyJS.onCommand = (user, command, message, flags, self, extra) => {
   }
 };
 
-ComfyJS.onChat = (user, message, flags, self, extra) => {
-  if (flags.customReward) {
-    Object.keys(gifz).forEach((element) => {
-      let idxCmd = Object.keys(gifz).indexOf(element);
-      if (extra.customRewardId === Object.values(gifz)[idxCmd].Id) {
-        displayGif(element, Object.values(gifz)[idxCmd].text);
-        return;
-      }
-    });
-  }
-};
-
 displayGif = function (name, btext) {
   console.log(name);
-  gif.src = `./assets/${name}.gif`;
-  sound.src = `./assets/${name}.mp3`;
+  gif.src = `https://raw.githubusercontent.com/Pieloaf/TwitchThings/master/gif-overlay/assets/${name}.gif`;
+  sound.src = `https://raw.githubusercontent.com/Pieloaf/TwitchThings/master/gif-overlay/assets/${name}.mp3`;
   sound.autoplay = true;
   text.innerHTML = btext;
   block.appendChild(gif);
